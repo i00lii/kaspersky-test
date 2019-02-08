a3c94cb5ca406079fcc189816216d931e0446699
daniil <daniil@webkontrol.ru>
2019-02-09 00:24:41 +0300

    Added string.isNullOrEmpty() extension method

---
+++ Kaspersky.Utils/Text/String.IsNullOrEmpty.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Kaspersky.Utils.Text
+{
+    public static partial class StringExtensions
+    {
+        public static bool IsNullOrEmpty( this string value ) => String.IsNullOrEmpty( value );
+    }
+}
